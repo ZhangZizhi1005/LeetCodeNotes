2742a91d1412857f9f6ff6ade4188a47f192bd78
zzz <zhangzzchn@126.com>
2021-02-02 21:50:37 +0800

    _0206

---
+++ LeetCode_Java/leetcode/Link/_0206/Solution.java
@@ -0,0 +1,16 @@
+package Link._0206;
+
+import Link._0234.ListNode;
+
+class Solution {
+    public ListNode reverseList(ListNode head){
+        if(head == null || head.next == null)
+            return head;
+
+        ListNode currentNode = head;
+
+        while (currentNode.next != null){
+
+        }
+    }
+}
