8f3922fd9f56cd73dc263b38e28077780ef2a113
zzz <zhangzzchn@126.com>
2021-02-03 15:45:49 +0800

    _0206

--- LeetCode_Java/leetcode/Link/_0206/Solution.java
+++ LeetCode_Java/leetcode/Link/_0206/Solution.java
@@ -3,14 +3,33 @@ package Link._0206;
 import Link._0234.ListNode;
 
 class Solution {
-    public ListNode reverseList(ListNode head){
+    public ListNode iterativeReverseList(ListNode head){
         if(head == null || head.next == null)
             return head;
 
         ListNode currentNode = head;
+        ListNode prev = null;
 
-        while (currentNode.next != null){
 
+        while (currentNode != null){
+            ListNode tempNext = currentNode.next;
+            currentNode.next = prev;
+            prev = currentNode;
+            currentNode = tempNext;
         }
+        return prev;
+    }
+
+    public ListNode recursiveReserveList(ListNode head){
+        if ( head == null || head.next == null ){
+            return head;
+        }
+
+        ListNode newHead = recursiveReserveList(head.next);
+
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
     }
 }
